1b7f80310048edc96417ee05a9e399820441da6b
Jobsidney <jobsidney67@gmail.com>
2022-06-02 10:31:32 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Write your solution here!
+function beforeEach(){}
+const cats=["Milo", "Otis", "Garfield"]
+// beforeEach(function () {
+//     cats.length = 0;
+  
+//     cats.push("Milo", "Otis", "Garfield");
+//   });
+
+function destructivelyAppendCat(name){
+    return cats.push(name);
+}
+destructivelyAppendCat("Milo");
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name);
+}
+destructivelyPrependCat("Bob");
+
+function destructivelyRemoveLastCat(){
+    return cats.pop();
+
+}
+destructivelyRemoveLastCat()
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+
+}
+destructivelyRemoveFirstCat();
+function appendCat(name){
+   return [...cats,name]
+}
+appendCat("Ralph");
+
+function prependCat(name){
+    return [name,...cats];
+
+}
+prependCat("Bob");
+
+
+function removeLastCat(){
+    
+    return cats.slice(0,-1);
+}
+removeLastCat();
+
+
+function removeFirstCat(){
+    return cats.slice(1);
+
+}
+removeFirstCat();
